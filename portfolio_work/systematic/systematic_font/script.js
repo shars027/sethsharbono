7c6a6b5bb72f70cb5f4e1b06d88fcd6535a60a1a
Seth Sharbono <SethSharbono@Seths-MacBook-Pro.local>
2018-10-09 01:35:18 -0400

    first commit

---
+++ portfolio_work/systematic/systematic_font/script.js
@@ -0,0 +1,216 @@
+$(document).ready(function(){
+
+    $(".name").hide();
+    $(document).keydown(function()
+    	{$(".message").hide();
+    });
+
+// A
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 65) {
+			$(".name").hide();
+			$("#amy_schumer").toggle();
+			$("#letters").css('background-color', 'rgb(0, 85, 166)');
+		}
+	});
+// B
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 66) {
+			$(".name").hide();
+			$("#bradley_cooper").toggle();
+			$("#letters").css('background-color', 'rgb(255, 232, 94)');
+		}
+	});
+// C
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 67) {
+			$(".name").hide();
+			$("#chelsea_handler").toggle();
+			$("#letters").css('background-color', 'rgb(229, 7, 18)');
+		}
+	});
+// D
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 68) {
+			$(".name").hide();
+			$("#ellen_degeneres").toggle();
+			$("#letters").css('background-color', 'rgb(21, 186, 229)');
+		}
+	});
+// E
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 69) {
+			$(".name").hide();
+			$("#emma_stone").toggle();
+			$("#letters").css('background-color', 'rgb(82, 42, 180)');
+		}
+	});	
+// F
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 70) {
+			$(".name").hide();
+			$("#tina_fey").toggle();
+			$("#letters").css('background-color', 'rgb(247, 190, 1)');
+		}
+	});
+// G
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 71) {
+			$(".name").hide();
+			$("#lady_gaga").toggle();
+			$("#letters").css('background-color', 'rgb(199, 218, 232)');
+		}
+	});
+// H
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 72) {
+			$(".name").hide();
+			$("#hugh_jackman").toggle();
+			$("#letters").css('background-color', 'rgb(30, 105, 160)');
+		}
+	});
+// I
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 73) {
+			$(".name").hide();
+			$("#imagine_dragons").toggle();
+			$("#letters").css('background-color', 'rgb(153, 129, 152)');
+		}
+	});
+// J
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 74) {
+			$(".name").hide();
+			$("#jake_gyllenhaal").toggle();
+			$("#letters").css('background-color', 'rgb(224, 223, 47)');
+		}
+	});
+// K
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 75) {
+			$(".name").hide();
+			$("#kesha").toggle();
+			$("#letters").css('background-color', 'rgb(205, 98, 138)');
+		}
+	});
+// L
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 76) {
+			$(".name").hide();
+			$("#jennifer_lawrence").toggle();
+			$("#letters").css('background-color', 'rgb(181, 24, 24)');
+		}
+	});
+// M
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 77) {
+			$(".name").hide();
+			$("#miley_cyrus").toggle();
+			$("#letters").css('background-color', 'rgb(229, 85, 175)');
+		}
+	});
+// N
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 78) {
+			$(".name").hide();
+			$("#nicole_richie").toggle();
+			$("#letters").css('background-color', 'rgb(102, 191, 225)');
+		}
+	});
+// O
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 79) {
+			$(".name").hide();
+			$("#onerepublic").toggle();
+			$("#letters").css('background-color', 'rgb(222, 227, 235)');
+		}
+	});
+// P
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 80) {
+			$(".name").hide();
+			$("#amy_poehler").toggle();
+			$("#letters").css('background-color', 'rgb(72, 177, 53)');
+		}
+	});
+// Q
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 81) {
+			$(".name").hide();
+			$("#queen_latifah").toggle();
+			$("#letters").css('background-color', 'rgb(117, 48, 184)');
+		}
+	});
+// R
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 82) {
+			$(".name").hide();
+			$("#margot_robbie").toggle();
+			$("#letters").css('background-color', 'rgb(232, 70, 127)');
+		}
+	});
+// S
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 83) {
+			$(".name").hide();
+			$("#taylor_swift").toggle();
+			$("#letters").css('background-color', 'rgb(207, 193, 160)');
+		}
+	});
+// T
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 84) {
+			$(".name").hide();
+			$("#tom_hanks").toggle();
+			$("#letters").css('background-color', 'rgb(0, 196, 242)');
+		}
+	});
+// U
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 85) {
+			$(".name").hide();
+			$("#carrie_underwood").toggle();
+			$("#letters").css('background-color', 'rgb(255, 211, 98)');
+		}
+	});
+// V
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 86) {
+			$(".name").hide();
+			$("#viola_davis").toggle();
+			$("#letters").css('background-color', 'rgb(131, 90, 168)');
+		}
+	});
+// W
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 87) {
+			$(".name").hide();
+			$("#mark_wahlberg").toggle();
+			$("#letters").css('background-color', 'rgb(80, 108, 141)');
+		}
+	});
+// X
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 88) {
+			$(".name").hide();
+			$("#x_ambass").toggle();
+			$("#letters").css('background-color', 'rgb(121, 115, 55)');
+		}
+	});
+// Y
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 89) {
+			$(".name").hide();
+			$("#young_thug").toggle();
+			$("#letters").css('background-color', 'rgb(165, 39, 27)');
+		}
+	});
+// Z
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 90) {
+			$(".name").hide();
+			$("#catherine_zeta_jones").toggle();
+			$("#letters").css('background-color', 'rgb(41, 99, 128)');
+		}
+	});
+});
---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	$("#alt_body , .alt").hide();
+
+	var x = 0;
+
+	$("#menu_icon").click(function(){
+		$("#alt_body").fadeToggle();
+		$(".alt").fadeToggle( 60 );
+		$("#logo_bar").toggleClass("expanded");
+		$("#logo").toggleClass("inverted");
+		$("#menu_icon").toggleClass("toggled");
+
+		if(x == 0) {
+			$("#icon").attr("src","logo/menu_icon_x.svg"); 
+			x = 1;
+		}
+		else if(x == 1) {
+			$("#icon").attr("src","logo/menu_icon.svg");
+			x = 0;
+	    };
+
+	});
+});
