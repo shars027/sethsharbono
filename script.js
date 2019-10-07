cabeab670c64bdf60468711343c6be57a1dc9224
shars027 <36018306+shars027@users.noreply.github.com>
2019-10-07 12:02:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 $(document).ready(function(){
+	$("#menu").click(function() {
+		$("#menu_box").toggle();
 
-	$("#alt_body , .alt").hide();
-
-	var x = 0;
-
-	$("#menu_icon").click(function(){
-		$("#alt_body").fadeToggle();
-		$(".alt").fadeToggle( 60 );
-		$("#logo_bar").toggleClass("expanded");
-		$("#logo").toggleClass("inverted");
-		$("#menu_icon").toggleClass("toggled");
-		$("#right_white_box").toggleClass("toggled");
-		$("#left_white_box").toggleClass("toggled");
-
-		if(x == 0) {
-			$("#icon").attr("src","logo/menu_icon_x.svg"); 
-			x = 1;
-		}
-		else if(x == 1) {
-			$("#icon").attr("src","logo/menu_icon.svg");
-			x = 0;
-	    };
-
+		var el = $(this);
+			if (el.text() == el.data("text-swap")) {
+				el.text(el.data("text-original"));
+			} else {
+				el.data("text-original", el.text());
+				el.text(el.data("text-swap"));
+			}
 	});
-
-
 });
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+	  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("slides");
+	if (n > x.length) {slideIndex = 1} 
+	if (n < 1) {slideIndex = x.length} ;
+	for (i = 0; i < x.length; i++) {
+	x[i].style.display = "none"; 
+	}
+	x[slideIndex-1].style.display = "block"; 
+}
