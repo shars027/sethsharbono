4fc8eef92630c713cf5c54bb510bb54005ea2e8b
shars027 <36018306+shars027@users.noreply.github.com>
2024-02-14 21:56:40 -0500

    update 14

---
+++ three/Row_01_03/script.js
@@ -0,0 +1,216 @@
+$(document).ready(function(){
+
+    $(".name").hide();
+    $(document).keydown(function()
+    	{$(".message").hide();
+    });
+
+// A
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 65) {
+			$(".name").hide();
+			$("#amy_schumer").toggle();
+			$("#letters").css('background-color', 'rgb(255, 233, 71)');
+		}
+	});
+// B
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 66) {
+			$(".name").hide();
+			$("#bradley_cooper").toggle();
+			$("#letters").css('background-color', 'rgb(88, 107, 164)');
+		}
+	});
+// C
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 67) {
+			$(".name").hide();
+			$("#chelsea_handler").toggle();
+			$("#letters").css('background-color', 'rgb(240, 58, 71)');
+		}
+	});
+// D
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 68) {
+			$(".name").hide();
+			$("#ellen_degeneres").toggle();
+			$("#letters").css('background-color', 'rgb(255, 113, 82)');
+		}
+	});
+// E
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 69) {
+			$(".name").hide();
+			$("#emma_stone").toggle();
+			$("#letters").css('background-color', 'rgb(112, 228, 233)');
+		}
+	});	
+// F
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 70) {
+			$(".name").hide();
+			$("#tina_fey").toggle();
+			$("#letters").css('background-color', 'rgb(255, 233, 71)');
+		}
+	});
+// G
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 71) {
+			$(".name").hide();
+			$("#lady_gaga").toggle();
+			$("#letters").css('background-color', 'rgb(88, 107, 164)');
+		}
+	});
+// H
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 72) {
+			$(".name").hide();
+			$("#hugh_jackman").toggle();
+			$("#letters").css('background-color', 'rgb(240, 58, 71)');
+		}
+	});
+// I
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 73) {
+			$(".name").hide();
+			$("#imagine_dragons").toggle();
+			$("#letters").css('background-color', 'rgb(255, 113, 82)');
+		}
+	});
+// J
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 74) {
+			$(".name").hide();
+			$("#jake_gyllenhaal").toggle();
+			$("#letters").css('background-color', 'rgb(112, 228, 233)');
+		}
+	});
+// K
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 75) {
+			$(".name").hide();
+			$("#kesha").toggle();
+			$("#letters").css('background-color', 'rgb(255, 233, 71)');
+		}
+	});
+// L
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 76) {
+			$(".name").hide();
+			$("#jennifer_lawrence").toggle();
+			$("#letters").css('background-color', 'rgb(88, 107, 164)');
+		}
+	});
+// M
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 77) {
+			$(".name").hide();
+			$("#miley_cyrus").toggle();
+			$("#letters").css('background-color', 'rgb(240, 58, 71)');
+		}
+	});
+// N
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 78) {
+			$(".name").hide();
+			$("#nicole_richie").toggle();
+			$("#letters").css('background-color', 'rgb(255, 113, 82)');
+		}
+	});
+// O
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 79) {
+			$(".name").hide();
+			$("#onerepublic").toggle();
+			$("#letters").css('background-color', 'rgb(112, 228, 233)');
+		}
+	});
+// P
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 80) {
+			$(".name").hide();
+			$("#amy_poehler").toggle();
+			$("#letters").css('background-color', 'rgb(255, 233, 71)');
+		}
+	});
+// Q
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 81) {
+			$(".name").hide();
+			$("#queen_latifah").toggle();
+			$("#letters").css('background-color', 'rgb(88, 107, 164)');
+		}
+	});
+// R
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 82) {
+			$(".name").hide();
+			$("#margot_robbie").toggle();
+			$("#letters").css('background-color', 'rgb(240, 58, 71)');
+		}
+	});
+// S
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 83) {
+			$(".name").hide();
+			$("#taylor_swift").toggle();
+			$("#letters").css('background-color', 'rgb(255, 113, 82)');
+		}
+	});
+// T
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 84) {
+			$(".name").hide();
+			$("#tom_hanks").toggle();
+			$("#letters").css('background-color', 'rgb(112, 228, 233)');
+		}
+	});
+// U
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 85) {
+			$(".name").hide();
+			$("#carrie_underwood").toggle();
+			$("#letters").css('background-color', 'rgb(255, 233, 71)');
+		}
+	});
+// V
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 86) {
+			$(".name").hide();
+			$("#viola_davis").toggle();
+			$("#letters").css('background-color', 'rgb(88, 107, 164)');
+		}
+	});
+// W
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 87) {
+			$(".name").hide();
+			$("#mark_wahlberg").toggle();
+			$("#letters").css('background-color', 'rgb(240, 58, 71)');
+		}
+	});
+// X
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 88) {
+			$(".name").hide();
+			$("#x_ambass").toggle();
+			$("#letters").css('background-color', 'rgb(255, 113, 82)');
+		}
+	});
+// Y
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 89) {
+			$(".name").hide();
+			$("#young_thug").toggle();
+			$("#letters").css('background-color', 'rgb(112, 228, 233)');
+		}
+	});
+// Z
+	$(document).keydown(function(keyPressed) {
+		if (keyPressed.keyCode == 90) {
+			$(".name").hide();
+			$("#catherine_zeta_jones").toggle();
+			$("#letters").css('background-color', 'rgb(255, 233, 71)');
+		}
+	});
+});
